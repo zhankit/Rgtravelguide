59ee96084d5a35c19b3b2653da094fbb560e2e00
zhankit <zhankit1209@gmail.com>
2018-08-05 00:59:56 +0800

    second-commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   get 'pages/about'
+  get 'pages/maps'
 
   root "destinations#index"
   resources :destinations
-  resources :destinations
+  resources :maps
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
